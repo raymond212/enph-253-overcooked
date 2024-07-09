d6b021e41e1f75f5cf78c2ace749607ed3a561c2
Raymond <ruiwenhw@gmail.com>
2024-07-08 22:33:02 -0700

    Add motor test with encoders 7/8

--- tests/motor_test/src/main.cpp
+++ tests/motor_test/src/main.cpp
@@ -1,25 +1,37 @@
 #include <Arduino.h>
 
-const int motorF = 21;
-const int motorB = 22;
-// const int motorE = 20;
-const int pot = 25;
+const int motorF = 22;
+const int motorB = 19;
+const int potPin = 37;
+const int encoder = 35;
+
+volatile int num = 0;
+
+void increment();
+
+void increment() {
+  num++;
+}
 
 void setup() {
+  Serial.begin(9600);
   pinMode(motorF, OUTPUT);
   pinMode(motorB, OUTPUT);
-  pinMode(pot, INPUT);
-  // pinMode(motorE, OUTPUT);
-  // analogWriteFrequency(2000);
+
+  pinMode(potPin, INPUT);
+  pinMode(encoder, INPUT_PULLUP);
+  attachInterrupt(encoder, increment, RISING);
+
+  analogWriteFrequency(250);
 }
 
 void loop() {
-  int speed = analogRead(pot);
+  int analogValue = analogRead(potPin);
 
-  analogWrite(motorF, map(speed, 0, 4095, 0, 255));
-  digitalWrite(motorB, LOW);
+  analogWrite(motorF, map(analogValue, 0, 4095, 0, 255));
+  analogWrite(motorB, 0);
 
-  analogWrite(motorB, 60);
-  digitalWrite(motorF, LOW);
-  delay(20);
+  Serial.println(num);
+  num = 0;
+  delay(1000);
 }
