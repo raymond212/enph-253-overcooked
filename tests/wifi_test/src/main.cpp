c5c0b4687646b27c7c3eeabc3fa1a430ce519294
Raymond <ruiwenhw@gmail.com>
2024-07-11 13:26:34 -0700

    Add OTA flash 7/11

--- tests/wifi_test/src/main.cpp
+++ tests/wifi_test/src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ArduinoOTA.h>
 
 const char* ssid = "Raymond";
 const char* password = "lithiumRanger";
+long lastOTATime = millis();
 
 WiFiServer server(23);
 WiFiClient remoteClient;
 
 void setup() {
   Serial.begin(115200);
-  
+
+  // Wifi setup
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -20,6 +23,34 @@ void setup() {
   server.begin();
   Serial.println("Server started");
   Serial.println("IP Address: " + WiFi.localIP());
+
+  // OTA setup
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else
+      type = "filesystem";
+
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
 }
 
 void loop() {
@@ -34,7 +65,13 @@ void loop() {
   }
 
   if (remoteClient.connected()) {
-    remoteClient.println("Hello World!");
+    remoteClient.println("Hello World! This is ESP32");
     delay(1000);
+  }
+
+  // OTA check
+  if (millis() - lastOTATime > 1000) {
+    ArduinoOTA.handle();
+    lastOTATime = millis();
   }
 }
