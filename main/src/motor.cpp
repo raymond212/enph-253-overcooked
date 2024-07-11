ac6a94f90761b5d59a73923953b4eddd9515a09d
youssefelbaz32 <107543132+youssefelbaz32@users.noreply.github.com>
2024-07-11 15:08:58 -0600

    Update motor.cpp

--- main/src/motor.cpp
+++ main/src/motor.cpp
@@ -5,6 +5,7 @@ Motor::Motor(int forwardPin, int backwardPin, int encoderPinA, int encoderPinB) 
   pinMode(backwardPin, OUTPUT);
   pinMode(encoderPinA, INPUT_PULLUP);
   pinMode(encoderPinB, INPUT_PULLUP);
+  lastResetTime = millis();
 }
 
 void Motor::stop() {
@@ -25,6 +26,7 @@ void Motor::updateEncoder() {
   } else {
     count--;
   }
+  resetEncoderPeriodically();
 }
 
 bool Motor::driveDistance(double targetDistance, double maxSpeed) {
@@ -33,9 +35,30 @@ bool Motor::driveDistance(double targetDistance, double maxSpeed) {
     return true;
   }
   double travelledDistance = getDistance();
-
-
   return false;
+}
+
+
+
+void Motor::setSpeed_PID(double speed) {
+  int target_encoders = speed / WHEEL_CIRCUMFERENCE_IN * 100 * 0.75 * 1141;
+  double KP = 0.02;
+  double KD = 0.01;
+  double KI = 0.005;
+  
+  double e1_error = polled_count - getCount();
+  speed += e1_error * KP + e_previous_error*KD + e_sum_error * KI;
+  speed = max(min(float(1), float(speed)), float(0));
+
+  e_previous_error = e1_error;
+  e_sum_error += e1_error;
+}
+
+
+
+
+double Motor::get_real_speed() {
+  return getDistance()/(millis() - lastResetTime); //inches per millisecond
 }
 
 void Motor::setSpeed(double speed) {
@@ -53,11 +76,20 @@ void Motor::setSpeed(double speed) {
 
 void Motor::resetEncoder() {
   noInterrupts();
+  polled_count = count;
   count = 0;
+  lastResetTime = millis();
   interrupts();
+}
+
+void Motor::resetEncoderPeriodically() {
+  if (millis() - lastResetTime >= 100) {
+    resetEncoder();
+  }
 }
 
 double Motor::mapd(double x, double in_min, double in_max, double out_min, double out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
