558e45a32ecd0fbfa9f0ccede6510b2de5379e57
Raymond <ruiwenhw@gmail.com>
2024-07-13 00:38:41 -0700

    Add bottom robot scraper servo testing 7/12

--- tests/servo_test/src/main.cpp
+++ tests/servo_test/src/main.cpp
@@ -5,16 +5,42 @@ Servo myservo;
 int servoPin = 13;
  
 void setup() {
+  Serial.begin(9600);
 	myservo.setPeriodHertz(50);
 	myservo.attach(servoPin, 500, 2500);
-  
+  Serial.println("Enter servo position: ");
   // analogWriteFrequency(50);
 }
  
+int curPosition = 140;
+
 void loop() {
-  int T = 1300;
-  myservo.write(0);
-  delay(T);
-  myservo.write(180);
-  delay(T);
+  // input scraper: 140 to 20 
+
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    // input.trim();
+    // int targetPosition = (input == "O") ? 140 : 20;
+    // // int targetPosition = input.toInt();
+    // Serial.println("Moving to position: " + String(targetPosition));
+    myservo.write(20);
+    delay(800);
+    myservo.write(140);
+    // delay();
+    // if (targetPosition >= 0 && targetPosition <= 180) {
+    //   myservo.write(targetPosition);
+    //   curPosition = targetPosition;
+    //   // while (curPosition != targetPosition) {
+    //   //   if (curPosition < targetPosition) {
+    //   //     curPosition++;
+    //   //   } else {
+    //   //     curPosition--;
+    //   //   }
+    //   //   myservo.write(curPosition);
+    //   //   delay(15);
+    //   // }
+    // } else {
+    //   Serial.println("Invalid position.");
+    // }
+  }
 }
