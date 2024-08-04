b6daa9393c12d3a27b8fff3beeed0c183e728aec
youssefelbaz32 <107543132+youssefelbaz32@users.noreply.github.com>
2024-08-04 16:14:19 -0600

    Create constants.h

---
+++ tests/communication_test/NEW_COMM/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//Lucas + Cathy + Bermel's: {0x64, 0xB7, 0x08, 0x9B, 0xB0, 0x38}; 
+
+
+// breadboard address: 64:b7:08:9c:5c:84
+//non-breadboard address: 64:b7:08:9c:5c:e4
+
+
+
+const uint8_t bottom_robot_address[] = {0x64, 0xB7, 0x08, 0x9C, 0x5D, 0x98}; //CHANGE THIS TO THE CORRECT MAC ADDRESS
+
+const uint8_t  top_robot_address[] = {0x64, 0xB7, 0x08, 0x9C, 0x5C, 0x84};
+//64:b7:08:9c:5c:c4
